72a19ec1e502d9528af012654522ca562e06d067
Ramees <rameeswork@gmail.com>
2025-01-25 16:31:21 +0530

    Day 2 completed with file upload functionality yooooooooooo

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -2,19 +2,46 @@
 
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import {
+  //   CheckCircleIcon,
+  CircleArrowDown,
+  //   HammerIcon,
+  RocketIcon,
+  //   SaveIcon,
+} from "lucide-react";
 
 const FileUploader = () => {
-  const onDrop = useCallback((acceptedFiles: File[]) => {}, []);
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    console.log(acceptedFiles);
+  }, []);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive, isFocused, isDragAccept } =
+    useDropzone({
+      onDrop,
+    });
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()}/>
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' Drop some files here, or click to select files</p>
-      )}
+    <div className=" flex flex-col gap-4 items-center max-w-7xl mx-auto">
+      <div
+        {...getRootProps()}
+        className={`p-10 border-indigo-600 text-indigo-600 border-2 border-dashed mt-10 w-[90%] rounded-lg h-96 flex items-center justify-center ${
+          isFocused || isDragAccept ? "bg-indigo-300" : "bg-indigo-100"
+        }`}
+      >
+        <input {...getInputProps()} />
+        <div className=" flex flex-col items-center justify-center">
+          {isDragActive ? (
+            <>
+              <RocketIcon className="h-20 w-20 animate-ping " />
+              <p>Drop the files here ...</p>
+            </>
+          ) : (
+            <>
+              <CircleArrowDown className="h-20 w-20 animate-bounce" />
+              <p>Drag n Drop some files here, or click to select files</p>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
