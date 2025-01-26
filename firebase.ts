f792e170cc7e03b30e2f9f60faad0cf40fbae986
Ramees <rameeswork@gmail.com>
2025-01-26 16:55:50 +0530

    firebase added 34:48

---
+++ firebase.ts
@@ -0,0 +1,20 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAitdrzRKounf9rnW1zqVXAPN05won1vIo",
+  authDomain: "chat-with-pdf-ed866.firebaseapp.com",
+  projectId: "chat-with-pdf-ed866",
+  storageBucket: "chat-with-pdf-ed866.firebasestorage.app",
+  messagingSenderId: "1011948412921",
+  appId: "1:1011948412921:web:af37f53f2778d6b985e728",
+  measurementId: "G-EP477DVYVW",
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
