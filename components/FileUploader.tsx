ba15d88842c101b0792c7131357125fc2e2c7759
Ramees <rameeswork@gmail.com>
2025-02-15 02:47:05 +0530

    implimenting AI Embedding for uploaded Files1:10

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -93,27 +93,29 @@ const FileUploader = () => {
           <p className="text-indigo-600 animate-pulse">{status}</p>
         </div>
       )}
-      <div
-        {...getRootProps()}
-        className={`p-10 border-indigo-600 text-indigo-600 border-2 border-dashed mt-10 w-[90%] rounded-lg h-96 flex items-center justify-center ${
-          isFocused || isDragAccept ? "bg-indigo-300" : "bg-indigo-100"
-        }`}
-      >
-        <input {...getInputProps()} />
-        <div className=" flex flex-col items-center justify-center">
-          {isDragActive ? (
-            <>
-              <RocketIcon className="h-20 w-20 animate-ping " />
-              <p>Drop the files here ...</p>
-            </>
-          ) : (
-            <>
-              <CircleArrowDown className="h-20 w-20 animate-bounce" />
-              <p>Drag n Drop some files here, or click to select files</p>
-            </>
-          )}
+      {!uploadProgress && (
+        <div
+          {...getRootProps()}
+          className={`p-10 border-indigo-600 text-indigo-600 border-2 border-dashed mt-10 w-[90%] rounded-lg h-96 flex items-center justify-center ${
+            isFocused || isDragAccept ? "bg-indigo-300" : "bg-indigo-100"
+          }`}
+        >
+          <input {...getInputProps()} />
+          <div className=" flex flex-col items-center justify-center">
+            {isDragActive ? (
+              <>
+                <RocketIcon className="h-20 w-20 animate-ping " />
+                <p>Drop the files here ...</p>
+              </>
+            ) : (
+              <>
+                <CircleArrowDown className="h-20 w-20 animate-bounce" />
+                <p>Drag n Drop some files here, or click to select files</p>
+              </>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
