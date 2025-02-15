7974d91ae3424277a2d21fec1a69cb42254b80a9
Ramees <rameeswork@gmail.com>
2025-02-16 02:49:32 +0530

    lange chain installion conflilict firebase-admin downgrade 13 to 12

--- actions/generateEmbedding.tsx
+++ actions/generateEmbedding.tsx
@@ -12,3 +12,6 @@ export async function generateEmbeddings(docId: string) {
   revalidatePath("/dashboard");
   return { completed: true };
 }
+
+
+
---
+++ lib/langchain.ts
@@ -0,0 +1,4 @@
+const model = new ChatOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  modelName: "gpt-4o",
+});
