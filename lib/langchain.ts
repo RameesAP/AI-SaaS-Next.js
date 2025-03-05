e7d53b9b7192164bbaae9cf25057295298b5caa6
Ramees <rameeswork@gmail.com>
2025-03-05 12:38:40 +0530

    using opensource AI with groq and embedding for cohere

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -1,4 +1,7 @@
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatGroq } from "@langchain/groq";
+import { CohereEmbeddings } from "@langchain/cohere";
+
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { OpenAIEmbeddings } from "@langchain/openai";
@@ -14,12 +17,26 @@ import { Index, RecordMetadata } from "@pinecone-database/pinecone";
 import { adminDb } from "../firebaseAdmin";
 import { auth } from "@clerk/nextjs/server";
 
-const model = new ChatOpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  modelName: "gpt-4o",
+
+const model = new ChatGroq({
+  apiKey: process.env.GROQ_API_KEY,
+  modelName: "llama-3.3-70b-versatile",
 });
 
-export const indexName = "chatwithpdf";
+
+//✅✅✅
+// const model = new ChatOpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+//   modelName: "gpt-4o",
+// });
+
+//✅✅✅
+//chatwithpdf is takes 1536 dimensions if you have open i key then use this
+// export const indexName = "chatwithpdf";
+
+
+// chatwithpdf2 is takes 1024 dimensions
+export const indexName = "chatwithpdf2";
 
 export async function generateDocs(docId: string) {
   const { userId } = await auth();
@@ -86,7 +103,17 @@ export async function generateEmbeddingsInPineconeVectorStore(docId: string) {
   //Generate embeddings { numerical representations} for the split document
   console.log(" ---- Generating embeddings.... ---- ");
 
-  const embeddings = new OpenAIEmbeddings();
+//✅✅✅
+//if you have the openai api key, you can use the openai embeddings
+  // const embeddings = new OpenAIEmbeddings();
+
+
+  const embeddings = new CohereEmbeddings({
+    apiKey: process.env.COHERE_API_KEY, // Use Cohere API Key
+    model: "embed-english-v3.0", 
+  });
+
+
 
   const index = await pineconeClient.index(indexName);
   const namespaceAlreadyExists = await namespaceExists(index, docId);
--- lib/pinecone.ts
+++ lib/pinecone.ts
@@ -1,11 +1,11 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 
-if (!process.env.PINECONE_API_KEY) {
+if (!process.env.PINECONE_API_KEY2) {
   throw new Error("PINECONE_API_KEY is not set");
 }
 
 const pineconeClient = new Pinecone({
-  apiKey: process.env.PINECONE_API_KEY,
+  apiKey: process.env.PINECONE_API_KEY2,
 });
 
 export default pineconeClient;
